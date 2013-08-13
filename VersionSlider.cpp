4511cf5d39a723cdd1a45320c57e1d6106ce4dbe
Alexis Murzeau <amurzeau@gmail.com>
2013-08-13 11:19:53 +0200

    Show timestamp in seconds of the current version since the first one

--- VersionSlider.cpp
+++ VersionSlider.cpp
@@ -1,6 +1,8 @@
 #include "VersionSlider.h"
 #include "ui_VersionSlider.h"
 
+#include "data_info/rpl_data.h"
+
 VersionSlider::VersionSlider(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::VersionSlider)
@@ -39,6 +41,9 @@ void VersionSlider::onChangeCurrentVersion(int newVersion) {
 	if(newVersion == 0) {
 		setValue(maximum());
 	} else setValue(newVersion - 1);
+
+	double timestamp = rpldata_wsn_version_get_timestamp(newVersion);
+	ui->currentTimeLabel->setText(QString::number(timestamp));
 }
 
 void VersionSlider::onUpdateVersionCount(int versionCount) {
@@ -49,13 +54,20 @@ void VersionSlider::onUpdateVersionCount(int versionCount) {
 	else setRealtime = false;
 
 	setMaximum(versionCount);
-	if(setRealtime)
+	if(setRealtime) {
 		setValue(versionCount);
+
+		double timestamp = rpldata_wsn_version_get_timestamp(0);
+		ui->currentTimeLabel->setText(QString::number(timestamp));
+	}
 }
 
 void VersionSlider::onVersionSliderMove(int value) {
 	int version = qMin(value, maximum()) + 1;
 	setValue(value);
+
+	double timestamp = rpldata_wsn_version_get_timestamp(version);
+	ui->currentTimeLabel->setText(QString::number(timestamp));
 
 	if(version > maximum())
 		version = 0;  //realtime mode
