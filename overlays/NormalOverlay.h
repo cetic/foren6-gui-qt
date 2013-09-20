97c979dcb24dc623f4931ae5178a34f3a06f755f
Laurent Deru <laurent.deru@cetic.be>
2013-09-20 10:40:27 +0200

    Display total errors count in normal overlay

--- overlays/NormalOverlay.h
+++ overlays/NormalOverlay.h
@@ -1,15 +1,12 @@
 #ifndef NORMALOVERLAY_H
 #define NORMALOVERLAY_H
 
-#include "../IOverlayModel.h"
+#include "ErrorsOverlay.h"
 
-class NormalOverlay : public IOverlayModel
+class NormalOverlay : public ErrorsOverlay
 {
 public:
-	virtual bool nodeCirclePen(rpl::Node *node, QPen *newPen, QBrush *newBrush);
-	virtual bool nodeTextPen(rpl::Node *node, QFont *newFont, QColor *newColor);
-	virtual bool linkPen(rpl::Link *link, QPen *newPen);
-    virtual bool nodeInfoText(rpl::Node *node, QString *  infoText);
+    virtual int getErrorCount(rpl::Node *node);
 };
 
 #endif // NORMALOVERLAY_H
