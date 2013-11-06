6afd465bd62d69bc2afd0a9541056138cd5bf79b
Laurent Deru <laurent.deru@cetic.be>
2013-11-06 16:51:25 +0100

    Links autoconf and single parent to settings and libanalyser

--- SettingsDialog.cpp
+++ SettingsDialog.cpp
@@ -15,11 +15,16 @@ SettingsDialog::SettingsDialog(QWidget *parent) :
 
     //Update config from user preferences
     QSettings settings;
-    settings.beginGroup("rpl");
-    config.root_rank = settings.value("root_rank", config.root_rank).toInt();
+    settings.beginGroup("6lowpan");
     QString context0 = settings.value("context0", ipv6string).toString();
     inet_pton(AF_INET6, qPrintable(context0), (void *)&config.context0);
-    //context
+    settings.endGroup();
+    settings.beginGroup("ipv6");
+    config.address_autconf_only = settings.value("address_autconf_only", config.address_autconf_only).toBool();
+    settings.endGroup();
+    settings.beginGroup("rpl");
+    config.one_preferred_parent = settings.value("one_preferred_parent", config.one_preferred_parent).toBool();
+    config.root_rank = settings.value("root_rank", config.root_rank).toInt();
     settings.endGroup();
 
     rpl_tool_set_analyser_config(&config);
@@ -41,30 +46,32 @@ void SettingsDialog::applySettings()
 
     config.root_rank = ui->settings_RplRootRankEdit->text().toInt(&valid, 10);
     valid = valid && inet_pton(AF_INET6, qPrintable(ui->settings_6lowpanContextEdit->text()), (void *)&config.context0);
-
+    config.address_autconf_only = ui->settings_AutoconfAddrsCbx->isChecked();
+    config.one_preferred_parent = ui->settings_SinglePreferredParentCbx->isChecked();
     if (valid) {
         rpl_tool_set_analyser_config(&config);
         //Update user preferences
         QSettings settings;
-        settings.beginGroup("rpl");
-        settings.setValue("root_rank", config.root_rank);
+        settings.beginGroup("6lowpan");
         settings.setValue("context0", ui->settings_6lowpanContextEdit->text());
         settings.endGroup();
+        settings.beginGroup("ipv6");
+        settings.setValue("address_autconf_only", config.address_autconf_only);
+        settings.endGroup();
+        settings.beginGroup("rpl");
+        settings.setValue("one_preferred_parent", config.one_preferred_parent);
+        settings.setValue("root_rank", config.root_rank);
+        settings.endGroup();
     }
-
-
 }
 
 void SettingsDialog::restoreSettings()
 {
     char ipv6string[INET6_ADDRSTRLEN];
-    bool autoconfAddresses, singlePreferredParent;
     const analyser_config_t *config = rpl_tool_get_analyser_config();
-    autoconfAddresses = true; //TODO: read from analyser config
-    singlePreferredParent = true; //TODO: read from analyser config
     ui->settings_RplRootRankEdit->setText(QString::number(config->root_rank));
     inet_ntop(AF_INET6, (const char*)&config->context0, ipv6string, INET6_ADDRSTRLEN);
     ui->settings_6lowpanContextEdit->setText(ipv6string);
-    ui->settings_AutoconfAddrsCbx->setChecked(autoconfAddresses);
-    ui->settings_SinglePreferredParentCbx->setChecked(singlePreferredParent);
+    ui->settings_AutoconfAddrsCbx->setChecked(config->address_autconf_only);
+    ui->settings_SinglePreferredParentCbx->setChecked(config->one_preferred_parent);
 }
